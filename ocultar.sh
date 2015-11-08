13054976dbdbf9812a58a07ecc060418621d5e64
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2015-11-08 13:18:22 +0100

    nueva imagen 4.5MB

--- ocultar.sh
+++ ocultar.sh
@@ -3,9 +3,3 @@ for f in output/50/*.jpg; do
   echo $f
   steghide embed -cf $f -ef inputOcultar/Prueba50  -p test
 done;
-
-
-IFS=$'\n'
-for f in output/150/*.jpg; do
-  steghide embed -cf $f -ef inputOcultar/Prueba150  -p test
-done
